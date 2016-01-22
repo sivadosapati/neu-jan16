0f8a0dabbf40ddeb65d8eaf4ab2a957356359313
yangneu2015 <liu.yang14@husky.neu.edu>
2016-01-22 10:43:04 -0800

    Assignment for lecture 2
    Deadline 01/27/2016 23:59.

---
+++ src/Assignment2.java
@@ -0,0 +1,95 @@
+/**
+ * Assignment for your lecture 2.
+ * Please finish all the questions under 'Assignment'.
+ * Please try the two questions under 'Bonus'.
+ * Please write some tests as practice.
+ * When you are writing tests, you should think about what situations might cause error? like 0,-1...
+ * The deadline of this assignment is 01/27/2016 23:59.
+ * Please feel free to contact me for any questions.
+ * @author Yang
+ */
+
+public class Assignment2 {
+	//Assignment
+	/**
+	 * Given an integer N as input, check the following:
+	 * If N is odd, print "Weird".
+	 * If N is even and, in between the range of 2 and 5(inclusive), print "Not Weird".
+	 * If N is even and, in between the range of 6 and 20(inclusive), print "Weird".
+	 * If N is even and N>20, print "Not Weird".
+	 */
+	public void weirdNumber(int n){
+		
+	}
+	
+	/**
+	 * Input an array of scores and return the right grades based on following rules
+	 * 90<=score    -->4
+	 * 80<=score<90 -->3
+	 * 70<=score<80 -->2
+	 * 60<=score<70 -->1
+	 * score<60     -->0
+	 */
+	public int[] checkGrade(int[] scores){
+		
+	}
+	
+	/**
+	 * Write a method to determine whether a number is prime 
+	 */
+	public boolean isPrimeNumber(int n){
+		
+	}
+	
+	/**
+	 * Write a method to find all numbers(<=n) which are power of three.
+	 */
+	public int[] findPowerOfThree(int n){
+		
+	}
+	
+	/**
+	 * Count occurrences of a given letter in a given string.
+	 */
+	public int countLetter(String str,char c){
+		
+	}
+	
+	/**
+	 * Find maximum and minimum in a given array, and return the max and min number
+	 */
+	public int[] findMinMax(int[] nums){
+		
+	}
+	
+	/**
+	 * Find N fibonacci numbers
+	 * Fibonacci Number: f(N) = f(N-1)+f(N-2).
+	 * Typically, f(0)=f(1)=1. 
+	 */
+	public int[] fibonacciNumber(int n){
+		
+	}
+	
+	
+	
+	//Bonus
+	/**
+	 * Given a non-negative integer n, repeatedly add all its digits until the result has only one digit.
+	 * For example:
+	 * Given n = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+	 */
+	public int addDigits(int n){
+		
+	}
+	
+	/**
+	 * Write a program to check whether a given number is an ugly number.
+	 * Ugly numbers are positive numbers whose prime factors only include 2, 3, 5.
+	 * For example, 6, 8 are ugly, while 14 is not ugly since it includes another prime factor 7.
+	 * Note that 1 is typically treated as an ugly number.
+	 */
+	public boolean isUgly(int n){
+		
+	}		
+}
