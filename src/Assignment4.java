2be6f09f342ed6a0842b6822064888c90974b509
yangneu2015 <liu.yang14@husky.neu.edu>
2016-02-08 12:57:55 -0800

    Assignment 4

---
+++ src/Assignment4.java
@@ -0,0 +1,76 @@
+/**
+ * Finish all the assignments. Try Bonus.
+ * @author Yang
+ */
+
+public class Assignment4 {
+	//Assignments
+	/**
+	 * Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+	 * If the last word does not exist, return 0.
+	 * Note: A word is defined as a character sequence consists of non-space characters only.
+	 */
+	public int lengthOfLastWord(String str){
+		
+	}
+	
+	/**
+	 * Reverse a string by letters. Do not use 'split' function.
+	 * e.g. "I like soccer" --> "reccos ekil I"
+	 */
+	public String reverseString1(String str){
+		
+	}
+	
+	/**
+	 * Reverse a string by words. Do not use 'split' function.
+	 * e.g. "I like soccer" --> "soccer like I"
+	 */
+	public String reverseString2(String str){
+		
+	}
+	
+	/**
+	 * Count how many words in a string. Numbers are regarded as a word. Do not use 'split' function.
+	 * e.g. "I like soccer" --> 3.    "I have 15 dollars" --> 4
+	 */
+	public int countWords(String str){
+		
+	}
+	
+	/**
+	 * Capitalize the first letters of words in a String. Do not use 'split' function.
+	 * e.g. "I like soccer" --> "I Like Soccer".
+	 */
+	public String capitalizeLetters(String str){
+		
+	}
+	
+	/**
+	 * Use enum to represent the type of vehicles, like Bus, Motorcycle, Sedan, SUV, Truck...
+	 */
+	
+	/**
+	 * Define a vehicle class. Use suitable encapsulation. Use the enum you defined to represent the types.
+	 * There is no standard answer for this question, but you need create necessary attributes for this class, and methods if needed.
+	 */
+	
+	
+	//Bonus
+	/**
+	 * Count and Say. Count each character.
+	 * e.g. "aabacbaa" --> "5a2b1c"
+	 */
+	public String countAndSay(String str){
+		
+	}
+	
+	/**
+	 * Compare version number. If equal, return 0. if larger, return 1. if smaller, return -1.
+	 * You could use library functions of Integer and String, like parseInt, split.
+	 * e.g. "1.2.3" VS "1.2.0" --> 1.     "1.2" VS "1.2.0" --> 0.      "100.200" VS "200.100" --> -1.
+	 */
+	public int compareVersion(String v1, String v2){
+		
+	}
+}
