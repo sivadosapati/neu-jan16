3f7b35924bfee346a827c17a79738c99ebbbbaea
Siva Dosapati <siva.dosapati@gmail.com>
2016-04-14 20:45:20 -0700

    More files..

---
+++ src/project/vehicle/management/dto/Car.java
@@ -0,0 +1,6 @@
+package project.vehicle.management.dto;
+
+public class Car {
+	private Integer year;
+	private String make;
+}
---
+++ src/project/vehicle/management/dto/Dealer.java
@@ -0,0 +1,6 @@
+package project.vehicle.management.dto;
+
+public class Dealer {
+	private String name;
+	private Inventory inventory;
+}
